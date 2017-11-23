7376694a455d50479591888b6e8061da6dc4d715
Onir27 <nirmalya_mahanti@infosys.com>
2017-11-23 15:42:11 +0530

    Adding Recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { nearer } from 'q';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('French Fry', 'Deep fried potato sticks', 'https://pixabay.com/en/french-fries-food-french-plate-1351062/'),
+  ];
   constructor() { }
 
   ngOnInit() {
